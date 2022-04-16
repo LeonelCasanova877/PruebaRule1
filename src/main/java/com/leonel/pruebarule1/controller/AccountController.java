7cf4f856d86b03b438f06d6a3789bd67de99bd1d
Leo <leonelcasanova877@gmail.com>
2022-04-15 23:05:53 -0400

    Added updateAccountState method and getAllAccounts

--- src/main/java/com/leonel/pruebarule1/controller/AccountController.java
+++ src/main/java/com/leonel/pruebarule1/controller/AccountController.java
@@ -1,33 +1,50 @@
 package com.leonel.pruebarule1.controller;
 
 import com.leonel.pruebarule1.model.Account;
-import com.leonel.pruebarule1.model.AccountType;
 import com.leonel.pruebarule1.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "accounts",consumes={"application/json"})
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
-    @GetMapping("accounts/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Account> getAccount(@PathVariable Long id) {
+
         return new ResponseEntity<>(accountService.findById(id), HttpStatus.OK);
     }
 
-    @PostMapping("accounts")
-    public ResponseEntity<String> createAccount(@RequestBody Account account){
+    @PostMapping
+    public ResponseEntity<String> createAccount(@RequestBody @Valid Account account){
+
         return new ResponseEntity<>("Account "+ accountService.createAccount(account)+" created", HttpStatus.CREATED);
     }
 
-    @GetMapping("accounts/listByType")
-    public ResponseEntity<List<Account>> getAccountsByType(@RequestBody AccountType accountType ) {
-        return new ResponseEntity<>(accountService.findAccountsByType(accountType), HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccountsByType(@RequestParam("accountTypeName") String accountTypeName ) {
+
+        return new ResponseEntity<>(accountService.findAccountsByType(accountTypeName), HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Account>> getAllAccounts() {
+
+        return new ResponseEntity<>(accountService.findAllAccounts(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateAccountState(@RequestParam("accountState") String accountStateName, @PathVariable Long id){
+
+        return new ResponseEntity<>("Account "+ accountService.updateAccountState(accountStateName, id)+" state updated ", HttpStatus.OK);
+    }
+
 }
