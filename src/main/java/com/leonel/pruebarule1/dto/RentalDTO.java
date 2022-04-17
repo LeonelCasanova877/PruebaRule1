e9e59a4523693185fe7fd44b85edd9b9bdb591d5
Leo <leonelcasanova877@gmail.com>
2022-04-17 02:47:57 -0400

    Remove account and accountType from Rental DTO

--- src/main/java/com/leonel/pruebarule1/dto/RentalDTO.java
+++ src/main/java/com/leonel/pruebarule1/dto/RentalDTO.java
@@ -11,8 +11,6 @@ public class RentalDTO implements Serializable {
     private LocalDateTime endDate;
     private String userEmail;
     private Long hours;
-    private String accountType;
-    private String account;
 
     public RentalDTO(Long price, LocalDateTime startDate, LocalDateTime endDate, String userEmail, Long hours, String accountType, String account) {
         this.price = price;
@@ -20,8 +18,14 @@ public class RentalDTO implements Serializable {
         this.endDate = endDate;
         this.userEmail = userEmail;
         this.hours = hours;
-        this.accountType = accountType;
-        this.account = account;
+    }
+
+    public RentalDTO(Long price, LocalDateTime startDate, LocalDateTime endDate, String userEmail, Long hours) {
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.userEmail = userEmail;
+        this.hours = hours;
     }
 
     public Long getPrice() {
@@ -64,32 +68,16 @@ public class RentalDTO implements Serializable {
         this.hours = hours;
     }
 
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RentalDTO rentalDTO = (RentalDTO) o;
-        return price.equals(rentalDTO.price) && startDate.equals(rentalDTO.startDate) && endDate.equals(rentalDTO.endDate) && userEmail.equals(rentalDTO.userEmail) && hours.equals(rentalDTO.hours) && accountType.equals(rentalDTO.accountType) && account.equals(rentalDTO.account);
+        return price.equals(rentalDTO.price) && startDate.equals(rentalDTO.startDate) && endDate.equals(rentalDTO.endDate) && userEmail.equals(rentalDTO.userEmail) && hours.equals(rentalDTO.hours);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(price, startDate, endDate, userEmail, hours, accountType, account);
+        return Objects.hash(price, startDate, endDate, userEmail, hours);
     }
 }
